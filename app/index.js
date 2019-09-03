50b86ce20ad160059969defd9cca6190de7efe60
EastblueOkay <eastblue_lan@126.com>
2019-09-03 22:54:18 +0800

    add webpack & dev-server

---
+++ app/index.js
@@ -0,0 +1,5 @@
+const r = () => {
+  console.log("babel webpack test");
+  return "Remini";
+};
+document.querySelector("#root").innerHTML = r();
---
+++ build/webpack.dev.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack')
+const { resolve } = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  context: __dirname,
+  entry: resolve(__dirname, '../app/index.js'),
+  output: {
+    path: resolve(__dirname, '../app'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, '../app'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  mode: 'development',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true,
+      }),
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
+}
